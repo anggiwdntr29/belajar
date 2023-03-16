fbe3147d2683b89fae6f76d37674626c9417428f
anggiwdntr29 <widiantara2906@gmail.com>
2023-03-16 15:34:18 +0800

    add screenPromo

--- src/components/Card/Promo/index.js
+++ src/components/Card/Promo/index.js
@@ -14,64 +14,56 @@ import { Warna } from "../../Style/index";
 const CardPromo = () => {
   return (
     <NativeBaseProvider>
-      <ScrollView
-        horizontal={true}
-        px={5}
-        showsHorizontalScrollIndicator={false}
-      >
-        <HStack space={3} mr={10}>
-          {Promo.map((Promo) => (
-            <Box
-              key={Promo.id}
+      {Promo.map((Promo) => (
+        <Box
+          key={Promo.id}
+          w={"160px"}
+          borderWidth={1}
+          borderColor={Warna.primary}
+          borderRadius={10}
+          padding={2}
+        >
+          <Center>
+            <Image
+              source={{ uri: `${Promo.image}` }}
+              alt={"img"}
               w={"160px"}
-              borderWidth={1}
-              borderColor={Warna.primary}
+              h={"100px"}
               borderRadius={10}
-              padding={2}
-            >
-              <Center>
-                <Image
-                  source={{ uri: `${Promo.image}` }}
-                  alt={"img"}
-                  w={"160px"}
-                  h={"100px"}
-                  borderRadius={10}
-                />
-              </Center>
-              <Text
-                mt={2}
-                h={"48px"}
-                fontSize={"sm"}
-                fontWeight={"medium"}
-                color={Warna.primary}
-              >
-                {Promo.title}
-              </Text>
-              <Text
-                fontSize={"sm"}
-                fontWeight={"light"}
-                strikeThrough
-                color={Warna.secondary}
-              >
-                {Promo.OldPrice}
-              </Text>
-              <Text fontSize={"md"} fontWeight={"medium"}>
-                {Promo.NewPrice}
-              </Text>
-              <Button
-                my={2}
-                borderRadius={10}
-                variant={"unstyled"}
-                bg={Warna.primary}
-              >
-                <Text fontWeight={"semibold"} color={Warna.putih}>
-                  Add to cart
-                </Text>
-              </Button>
-            </Box>
-          ))}
-        </HStack>
-      </ScrollView>
+            />
+          </Center>
+          <Text
+            mt={2}
+            h={"48px"}
+            fontSize={"sm"}
+            fontWeight={"medium"}
+            color={Warna.primary}
+          >
+            {Promo.title}
+          </Text>
+          <Text
+            fontSize={"sm"}
+            fontWeight={"light"}
+            strikeThrough
+            color={Warna.secondary}
+          >
+            {Promo.OldPrice}
+          </Text>
+          <Text fontSize={"md"} fontWeight={"medium"}>
+            {Promo.NewPrice}
+          </Text>
+          <Button
+            my={2}
+            borderRadius={10}
+            variant={"unstyled"}
+            bg={Warna.primary}
+          >
+            <Text fontWeight={"semibold"} color={Warna.putih}>
+              Add to cart
+            </Text>
+          </Button>
+        </Box>
+      ))}
     </NativeBaseProvider>
   );
 };
--- src/screen/Promo/index.js
+++ src/screen/Promo/index.js
@@ -1,10 +1,82 @@
-import { NativeBaseProvider, Text } from "native-base";
+import {
+  Box,
+  Flex,
+  Image,
+  NativeBaseProvider,
+  Center,
+  Text,
+  Button,
+} from "native-base";
 import React from "react";
+import { Dimensions } from "react-native";
+import listPromo from "../../components/Data/listPromo";
+import { Warna } from "../../components/Style";
+
+const screenWidth = Dimensions.get("window").width;
 
 const ScreenPromo = () => {
   return (
     <NativeBaseProvider>
-      <Text>promo</Text>
+      <Flex
+        borderWidth={1}
+        w={screenWidth}
+        p={5}
+        flex={1}
+        flexWrap={"wrap"}
+        flexDirection={"row"}
+        justify={"space-between"}
+      >
+        {listPromo.map((Promo) => (
+          <Box
+            key={Promo.id}
+            w={"160px"}
+            borderWidth={1}
+            borderColor={Warna.primary}
+            borderRadius={10}
+            padding={2}
+          >
+            <Center>
+              <Image
+                source={{ uri: `${Promo.image}` }}
+                alt={"img"}
+                w={"160px"}
+                h={"100px"}
+                borderRadius={10}
+              />
+            </Center>
+            <Text
+              mt={2}
+              h={"48px"}
+              fontSize={"sm"}
+              fontWeight={"medium"}
+              color={Warna.primary}
+            >
+              {Promo.title}
+            </Text>
+            <Text
+              fontSize={"sm"}
+              fontWeight={"light"}
+              strikeThrough
+              color={Warna.secondary}
+            >
+              {Promo.OldPrice}
+            </Text>
+            <Text fontSize={"md"} fontWeight={"medium"}>
+              {Promo.NewPrice}
+            </Text>
+            <Button
+              my={2}
+              borderRadius={10}
+              variant={"unstyled"}
+              bg={Warna.primary}
+            >
+              <Text fontWeight={"semibold"} color={Warna.putih}>
+                Add to cart
+              </Text>
+            </Button>
+          </Box>
+        ))}
+      </Flex>
     </NativeBaseProvider>
   );
 };
