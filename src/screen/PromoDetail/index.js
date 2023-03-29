248dafbef261cc1877d2c534928c5de48a45c6f6
anggiwdntr29 <widiantara2906@gmail.com>
2023-03-29 16:48:41 +0800

    comm

---
+++ src/screen/PromoDetail/index.js
@@ -0,0 +1,12 @@
+import { NativeBaseProvider, Text } from "native-base";
+import React from "react";
+
+const ScreenPromoDetail = () => {
+  return (
+    <NativeBaseProvider>
+      <Text>test</Text>
+    </NativeBaseProvider>
+  );
+};
+
+export default ScreenPromoDetail;
